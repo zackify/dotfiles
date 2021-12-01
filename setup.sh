ed969c5e1c64fb0a2ce1889ab3cb57463a010f3c
Zach Silveira <zackify@gmail.com>
2021-12-01 17:40:07 -0500

    test

--- setup.sh
+++ setup.sh
@@ -1,8 +1,2 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp -R /root/.oh-my-zsh /home/$USERNAME
-cp /root/.zshrc /home/$USERNAME
-chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
-
-
 mkdir -p "$HOME/.zsh"
 git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
